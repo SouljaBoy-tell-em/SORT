b254c34e83ee7f839a7eebc1dfa2b12aa7b325e6
Big Bill <60592559+sashascul@users.noreply.github.com>
2022-09-18 23:33:59 +0600

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -2,64 +2,83 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <string.h>
-
-
-#define LEN 50
+#include <ctype.h>
+#include <assert.h>
 
 
 void openStatus (FILE * file);
 unsigned long fileSize (FILE * file, struct stat buf);
 void statusMemory (char * mem);
 unsigned long amountOfString (char * mem);
-
-
-typedef struct list {
-
-    char str [LEN];
-    struct list * next;
-
-} List;
-
+int comp (const void * aptr, const void * bptr);
 
 
 int main (void) {
 
     struct stat buf;
-    FILE * file = fopen ("sort.txt", "rb");
-    char * mem_start = NULL;
+    FILE * file = fopen ("sort1.txt", "r");
     openStatus (file);
     unsigned long filesize = fileSize (file, buf);
-    mem_start = (char * ) calloc (sizeof (char), filesize);
+    char * mem_start = (char * ) calloc (sizeof (char), filesize);
     statusMemory (mem_start);
-
     fread (mem_start, sizeof (char), filesize, file);
     unsigned long amount_of_string = amountOfString (mem_start);
-
     char * str [amount_of_string];
-
-    int j = 1;
-    str[0] = mem_start;
-    while (*mem_start != EOF) {
-
-        if (*(mem_start - 1) == '\n') {
-
-            * (mem_start - 1) = '\0';
-            str[j] = mem_start;
-            j++;
-        }
-
-        mem_start++;
-    }
-
-    for (int i = 0; i < amount_of_string; i++) {
-
-        puts (str[i]);
-        putchar ('\n');
-
-    }
-
+    int j = 0;
+    
+    //qsort (str, amount_of_string, sizeof (char *), comp);
+	
+	for (int i = 0; i < strlen (mem_start); i++) {
+		
+		
+		if (mem_start[i] == '\n') {
+			
+			mem_start[i] = '\0';
+			str[j] = mem_start;
+			j++;
+		}
+		
+		mem_start++;
+		
+	}
+	
+	puts (str[0]);
+	
+    
     return 0;
 }
+
+
+int comp (const void * aptr, const void * bptr)
+{
+	
+	const char * str1 = * (const char ** ) aptr;
+	const char * str2 = * (const char ** ) bptr;
+
+    assert(str1);
+    assert(str2);
+
+    int i = 0;
+    int j = 0;
+    
+    while(1)
+    {   
+        while(!isalpha(str1[i]) && (str1[i] != '\0')) 
+        {
+            i++;
+        }
+        while(!isalpha(str2[j]) && (str2[j] != '\0'))
+        {
+            j++;
+        }
+        if(str1[i] != str2[j]) break;
+        if(str1[i] == '\0') break;
+        i++;
+        j++;
+    }
+    return(str1[i] - str2[j]);
+}
+
 
 
 void openStatus (FILE * file) {
@@ -92,7 +111,7 @@ unsigned long fileSize (FILE * file, struct stat buf) {
 
 unsigned long amountOfString (char * mem) {
 
-    int i = 0, amount = 0;
+    unsigned long i = 0, amount = 0;
 
     for (i = 0; i < strlen (mem); i++)
         if (*(mem + i) == '\n')
